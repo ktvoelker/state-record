ad600cc8d901200aed43887ae4243234270bb5ef
Karl Voelker <ktvoelker@gmail.com>
2011-04-27 01:22:28 -0500

    Added StateTField

--- src/Data/Record/StateFields.hs
+++ src/Data/Record/StateFields.hs
@@ -3,6 +3,8 @@ module Data.Record.StateFields
   ( IdField(..)
   , StateField(..)
   , idStateField
+  , StateTField(..)
+  , idStateTField
   , ReaderField(..)
   , idReaderField
   , FieldPath(..)
@@ -33,6 +35,19 @@ idStateField f =
   StateField
   { getFieldM = gets $ getField f
   , putFieldM = \b -> get >>= \a -> put $ putField f b a
+  }
+
+-- | A field descriptor with stateful and other effects.
+data StateTField m a b = StateTField
+  { getFieldT :: StateT a m b
+  , putFieldT :: b -> StateT a m ()
+  }
+
+-- | Make a StateTField from an IdField.
+idStateTField f =
+  StateTField
+  { getFieldT = gets $ getField f
+  , putFieldT = \b -> get >>= \a -> put $ putField f b a
   }
 
 -- | A field descriptor which operates in an environment.
@@ -67,6 +82,12 @@ instance FieldPath (IdField a b) (StateField b c) (StateField a c) where
 instance FieldPath (StateField a b) (IdField b c) (StateField a c) where
   ab // bc = ab // idStateField bc
 
+instance (Monad m) => FieldPath (IdField a b) (StateTField m b c) (StateTField m a c) where
+  ab // bc = (idStateTField ab :: StateTField m a b) // bc
+
+instance (Monad m) => FieldPath (StateTField m a b) (IdField b c) (StateTField m a c) where
+  ab // bc = ab // (idStateTField bc :: StateTField m b c)
+
 instance FieldPath (IdField a b) (ReaderField b c) (ReaderField a c) where
   ab // bc = idReaderField ab // bc
 
@@ -80,6 +101,14 @@ instance FieldPath (StateField a b) (StateField b c) (StateField a c) where
     , putFieldM = enter ab . putFieldM bc
     }
 
+instance (Monad m) =>
+  FieldPath (StateTField m a b) (StateTField m b c) (StateTField m a c) where
+  ab // bc =
+    StateTField
+    { getFieldT = enterT ab $ getFieldT bc
+    , putFieldT = enterT ab . putFieldT bc
+    }
+
 instance FieldPath (ReaderField a b) (ReaderField b c) (ReaderField a c) where
   ab // bc =
     ReaderField
@@ -87,24 +116,28 @@ instance FieldPath (ReaderField a b) (ReaderField b c) (ReaderField a c) where
     }
 
 -- | The class of field descriptors usable with a State monad.
-class FieldState f where
-  getf :: (MonadState a m) => f a b -> m b
-  putf :: (MonadState a m) => f a b -> b -> m ()
+class (MonadState a m) => FieldState f a m | f -> a where
+  getf :: f b -> m b
+  putf :: f b -> b -> m ()
 
-instance FieldState IdField where
+instance (MonadState a m) => FieldState (IdField a) a m where
   getf = gets . getField
   putf f b = get >>= put . putField f b
 
-instance FieldState StateField where
+instance (MonadState a m) => FieldState (StateField a) a m where
   getf = getFieldM
   putf = putFieldM
 
+instance (Monad m) => FieldState (StateTField m a) a (StateT a m) where
+  getf = getFieldT
+  putf = putFieldT
+
 -- | Modify the value of a field by applying a function.
-modf :: (FieldState f, MonadState a m) => f a b -> (b -> b) -> m ()
+modf :: (FieldState f a m, MonadState a m) => f b -> (b -> b) -> m ()
 modf f g = getf f >>= putf f . g
 
 -- | Run a stateful computation with a field as the state.
-enter :: (FieldState f, MonadState a m) => f a b -> State b x -> m x
+enter :: (FieldState f a m, MonadState a m) => f b -> State b x -> m x
 enter f m = do
   b <- getf f
   let (x, b') = runState m b
@@ -113,7 +146,7 @@ enter f m = do
 
 -- | Like 'enter', but allows the subcomputation to share the underlying
 --   monad of the enclosing computation.
-enterT :: (Monad m, FieldState f) => f a b -> StateT b m x -> StateT a m x
+enterT :: (Monad m, FieldState f a (StateT a m)) => f b -> StateT b m x -> StateT a m x
 enterT f m = do
   b <- getf f
   (x, b') <- lift $ runStateT m b
